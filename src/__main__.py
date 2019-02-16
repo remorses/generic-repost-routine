dcff18727cb2e484d669ff7d428569596889653a
remorses <beats.by.morse@gmail.com>
2019-02-16 19:45:27 +0100

    s

--- src/__main__.py
+++ src/__main__.py
@@ -6,8 +6,20 @@ import os
 from instabotnet import execute
 from .support import load_json, load_raw, merge, randomize, dotdict
 import random
+import dhash
+import requests
 import time
+import json
 
+def download(url):
+    response = requests.get(url, stream=True)
+    return response.raw
+
+def make_hash(data):
+    from PIL import Image
+    image = Image.open(data)
+    row, col = dhash.dhash_row_col(image)
+    return dhash.format_hex(row, col)
 
 if __name__ == '__main__':
     data_path = os.environ['GENOTYPE_FILE_PATH']
@@ -30,10 +42,20 @@ if __name__ == '__main__':
 
     print('starting routine')
 
-    execute(
+    result = execute(
         load_raw('src/routine.yml'),
         fenotype
     )
+
+
+    try:
+        url = result[-1]['url']
+        image = download(url)
+        genotype['media_posted'] += [make_hash(image)]
+    except:
+        print('no media posted')
+
+    json.dump(genotype, data_path)
 
     # time.sleep(10)
     #
